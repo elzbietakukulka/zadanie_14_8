8347da35d0b49c0b972c94fd5f0e5e4eaf6a6035
Elżbieta Kukułka <ellakaras@gmail.com>
2018-04-06 22:19:28 +0200

    Fix bug

--- components/Search.js
+++ components/Search.js
@@ -24,7 +24,7 @@ Search = React.createClass({
 
         render: function() {
             var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
-        };
+            
 
             return ( 
                 <input
